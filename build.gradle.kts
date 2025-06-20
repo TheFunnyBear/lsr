73139128e5a7fb19417d61b7ffd85a22699930de
Denis Kosolapov <denis@kosolapov.info>
2025-06-20 10:50:06 +0300

    Some changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,22 @@
-dependencies {
-    implementation("org.jsoup:jsoup:1.15.4")
-    implementation("org.xerial:sqlite-jdbc:3.45.1.0")
+plugins {
+    kotlin("jvm") version "1.9.23"
+    application
 }
 
 repositories {
     mavenCentral()
+}
+
+dependencies {
+    implementation("com.microsoft.playwright:playwright:1.44.0")
+    implementation("org.jsoup:jsoup:1.15.4")
+    implementation("org.xerial:sqlite-jdbc:3.45.1.0")
+}
+
+application {
+    mainClass.set("LsrKt") // имя класса с main функцией (файл Lsr.kt)
+}
+
+tasks.withType<JavaExec> {
+    jvmArgs = listOf("--add-opens=java.base/java.lang=ALL-UNNAMED")
 }
